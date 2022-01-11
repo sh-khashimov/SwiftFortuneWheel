883669528b6249cf9f5c9edf91e9b581876f6e40
Sherzod Khashimov <sh.khashimov@gmail.com>
2022-01-11 14:18:15 +0500

    SPM warning fix

--- Package.swift
+++ Package.swift
@@ -23,11 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftFortuneWheel",
-            dependencies: [],
-            exclude: ["Examples", "Documentation", "Images", "docs",]),
+            dependencies: []),
         .testTarget(
             name: "SwiftFortuneWheelTests",
-            dependencies: ["SwiftFortuneWheel"],
-            exclude: ["Examples", "Documentation", "Images", "docs",]),
+            dependencies: ["SwiftFortuneWheel"]),
     ]
 )
